ace8e2dc070d177b426af9d8102c3937c76b1828
Lucas Nardon <lucaslnardon@gmail.com>
2025-05-28 23:39:50 -0300

    update dmenu position

--- dmenu-play-music.sh
+++ dmenu-play-music.sh
@@ -3,7 +3,7 @@
 FOLDER=~/Music
 
 playMusic(){
-	NAME="$(find "$FOLDER" -type f | dmenu -i -b -l 15)" || exit 0
+	NAME="$(find "$FOLDER" -type f | dmenu -i -l 15)" || exit 0
 	ffplay -x 128 -y 128 "$NAME" >/dev/null
 }
 
