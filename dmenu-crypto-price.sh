7fcd888ce4b23791b911341bffd661690ea4fffe
Lucas Nardon <lucaslnardon@gmail.com>
2025-06-01 00:29:37 -0300

    refactor

--- dmenu-crypto-price.sh
+++ dmenu-crypto-price.sh
@@ -8,22 +8,18 @@ fi
 
 url="https://api.coingecko.com/api/v3/coins/$coin"
 response=$(curl -s "$url")
-if echo "$response" | jq -e '.error' >/dev/null; then
-  error_msg=$(echo "$response" | jq -r '.error')
-  notify-send "Crypto Info" "Error: $error_msg"
-  exit 1
-fi
+echo "$response" | jq -e '.error' >/dev/null && error_msg=$(echo "$response" | jq -r '.error') && notify-send "Crypto Info" "Error: $error_msg" && exit 1
 
 coin_name=$(echo "$response" | jq -r '.name')
 price_usd_raw=$(echo "$response" | jq -r '.market_data.current_price.usd')
 price_usd=$(printf "%'.2f" "$price_usd_raw")
 change_14h=$(echo "$response" | jq -r '.market_data.price_change_percentage_24h')
 img_url=$(echo "$response" | jq -r '.image.small')
-msg="Name: $coin_name \nPrice: \$${price_usd} \n24h Change: ${change_14h}%"
+msg="Price: \$${price_usd} \n24h Change: ${change_14h}%"
 
 tmp_img=$(mktemp --suffix=.png)
 curl -s -o "$tmp_img" "$img_url"
 
-notify-send -i "$tmp_img" "Crypto Info" "$msg"
+notify-send -i "$tmp_img" "$coin_name" "$msg"
 
 rm "$tmp_img"
